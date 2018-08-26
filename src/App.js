0e66c5d0e4ba4b13a05e07825438784737f61d14
John Pawlak <johnpawlak@gmail.com>
2018-08-26 09:32:13 -0400

    Register and Sign Out conditional rendering logic implemented

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 
 import './App.css';
@@ -33,6 +34,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -67,27 +69,39 @@ class App extends Component {
   .catch(err => console.log(err));
    }
 
-  onRouteChange = () => {
-    this.setState({route: 'home'});
+  onRouteChange = (route) => {
+    if (route ==='signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
  
 
   render() {
+    const { isSignedIn, imageUrl, box, route } = this.state;
     return (
       <div className="App">
         <Particles className='particles'
               params={particlesOptions}
             />
-        <Navigation />
-        { this.state.route === 'signin'
-            ? <Signin onRouteChange = {this.onRouteChange} />
-            : <div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { route === 'home'
+            ? <div>
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={box} imageUrl={imageUrl} />
         </div>
+        : (
+          route === 'signin' 
+          ? <Signin onRouteChange = {this.onRouteChange} />
+          : <Register onRouteChange = {this.onRouteChange} />
+
+        )
+        
         }
         
       </div>
